33c3a10f7cd03de0d3995391f04bb5d4d808b5b9
Felipe Suknaic <fsuknaic@hotmail.com>
2023-01-29 14:18:15 -0500

    aplicacao de DDD e entidade notificacao com getters e setters

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { NotificationService } from './notification.service';
+import { NotificationService } from '../notification.service';
 import { PrismaService } from './prisma.service';
 @Module({
   imports: [],
